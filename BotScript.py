c9f4887344aa65425880020321c76e967ec013c2
David Ramirez <ramirdav@oregonstate.edu>
2018-06-21 17:38:52 -0700

    m

--- BotScript.py
+++ BotScript.py
@@ -393,7 +393,7 @@ def main():
     dp.add_handler(CommandHandler("unquiet", unquiet))
 
     dp.add_handler(MessageHandler([Filters.status_update], empty_message))
-    dp.add_handler(MessageHandler([Filters.text], stats))
+    #dp.add_handler(MessageHandler([Filters.text], stats))
 
     dp.add_error_handler(error)
 
